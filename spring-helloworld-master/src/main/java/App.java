568b3f595ad3077de7ed8f31915b908286eee983
Dron4000 <90280844+Dron4000@users.noreply.github.com>
2021-12-26 19:09:31 +0300

    Update App.java

--- spring-helloworld-master/src/main/java/App.java
+++ spring-helloworld-master/src/main/java/App.java
@@ -4,15 +4,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
-        HelloWorld bean2 =applicationContext.getBean(HelloWorld.class);
-        System.out.println( bean==bean2);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean( "helloworld" );
+        System.out.println( bean.getMessage() );
+        HelloWorld bean2 = applicationContext.getBean(HelloWorld.class);
+        System.out.println( bean == bean2);
 
 
         Cat cat = applicationContext.getBean(Cat.class);
         Cat cat2 = applicationContext.getBean(Cat.class);
-        System.out.println(cat==cat2);
+        System.out.println(cat == cat2);
 
 
 
